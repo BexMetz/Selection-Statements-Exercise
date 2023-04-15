6f11f7b1c8b1446e742cd8614405ea1350459563
Bex <bexmetzger@gmail.com>
2023-04-15 13:04:03 -0700

    guessing game

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var favoriteNumber = 47;
+
+            Console.WriteLine("Lets play a guessing game!");
+            Console.WriteLine("Think you can guess my favorite number?");
+            Console.WriteLine("I'll give you a hint: its between 1 and 100");
+
+            int guess;
+            do
+            {
+                Console.WriteLine("Your guess?");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess > favoriteNumber)
+                {
+                    Console.WriteLine("Oh my! Your guess is too high!");
+                }
+                else if (guess < favoriteNumber)
+                {
+                    Console.WriteLine("Oh no! Your guess is too low!");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it! I'm very impressed!");
+                }
+            } while (guess != favoriteNumber);
         }
     }
+
 }
